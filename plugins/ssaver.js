23f8f03a220fdf73d4d989ecad258c042309fce6
STAR-KING0 <kingdavid23565@gmail.com>
2024-07-30 20:16:41 +0100

    Update ssaver.js

--- plugins/ssaver.js
+++ plugins/ssaver.js
@@ -138,7 +138,7 @@ smd(
     filename: __filename,
   },
   async (message) => {
-    const SupportMsg = `Support QUEEN ALYA\n\n OPay Bank: OPay Digital Services Limited(OPay)\n\n OPay Account: 8078144253 Name: ADENIKE AYOADE MARY\n\n ${Config.botname} *Nigeria Show Love*`;
+    const SupportMsg = `Support QUEEN ALYA\n\n OPay Bank: OPay Digital Services Limited(OPay) \n\n OPay Account: 8078144253 Name: ADENIKE AYOADE MARY\n\n ${Config.botname} *Nigeria Show Love*`;
 
     const contextInfo = {
       forwardingScore: 999,
