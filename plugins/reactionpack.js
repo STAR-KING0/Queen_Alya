f671d53a09849c7aef1d36d6a3512f2b2952ee22
STAR-KING0 <kingdavid23565@gmail.com>
2024-09-01 11:16:21 +0100

    Add files via upload

--- plugins/reactionpack.js
+++ plugins/reactionpack.js
@@ -1,288 +1,51 @@
-const { smd, sendAnimeReaction } = require("../lib");
+const {
+  smd,
+  tlang,
+  prefix,
+  Config,
+  sleep,
+  getBuffer,
+  astroJson,
+  smdBuffer
+} = require("../lib");
 
-smd(
-  {
-    pattern: "poke",
-    category: "reaction",
-    use: "<quote|reply|tag>",
-    filename: __filename,
-    desc: "send Anime poke reaction.",
-  },
-  async (bot, text, { cmdName }) => {
-    await sendAnimeReaction(bot, cmdName, "poked to", "poked to everyone.");
+smd({
+  'cmdname': "react",
+  'alias': ['reaction', "waifu"],
+  'category': "reaction",
+  'use': "[text]",
+  'info': "Searches reaction gif"
+}, async (message, text) => {
+  try {
+    // List of valid cmdnames as API endpoints
+    const validEndpoints = [
+      "poke", "hug", "hold", "hifi", "bite", "blush", "punch", "pat",
+      "kiss", "kill", "happy", "dance", "yeet", "wink", "slap", "bonk",
+      "bully", "cringe", "cuddle"
+    ];
+
+    // Check if a valid search term is provided
+    if (!text || !validEndpoints.includes(text)) {
+      return message.reply("Please provide a valid search term from the following: " + validEndpoints.join(', '));
+    }
+
+    // Fetch the gif from waifu API
+    const axios = require("axios");
+    const response = await axios.get("https://api.waifu.pics/sfw/" + text).catch(() => {});
+
+    // If no gif is found, reply with an error
+    if (!response.data || !response.data.url) {
+      return message.reply("*Could not find any gif*");
+    }
+
+    // Send the gif with a caption
+    message.send(response.data.url, {
+      'caption': Config.caption,
+      'gifPlayback': true
+    }, "amdvid", message);
+
+  } catch (error) {
+    // Log the error and notify the user
+    message.error(`${error}\n\nCommand: react`, error, "*Could not find any gif*");
   }
-);
-//-----------------------------------------------------------------------
-smd(
-  {
-    pattern: "hug",
-    category: "reaction",
-    use: "<quote|reply|tag>",
-    filename: __filename,
-    desc: "send Anime hug reaction.",
-  },
-  async (bot, text, { cmdName }) => {
-    await sendAnimeReaction(bot, cmdName, "hug to", "hug with everyone.");
-  }
-);
-//-----------------------------------------------------------------------
-smd(
-  {
-    pattern: "hold",
-    category: "reaction",
-    use: "<quote|reply|tag>",
-    filename: __filename,
-    desc: "send Anime hand hold reaction.",
-  },
-  async (bot, text, { cmdName }) => {
-    await sendAnimeReaction(
-      bot,
-      "handhold",
-      "hold hand of",
-      "holded hand of everyone"
-    );
-  }
-);
-//-----------------------------------------------------------------------
-smd(
-  {
-    pattern: "hifi",
-    category: "reaction",
-    use: "<quote|reply|tag>",
-    filename: __filename,
-    desc: "send Anime hifi reaction.",
-  },
-  async (bot, text, { cmdName }) => {
-    await sendAnimeReaction(
-      bot,
-      "highfive",
-      "highfive with",
-      "highfive with everyone."
-    );
-  }
-);
-//---------------------------------------------------------------------------
-smd(
-  {
-    pattern: "bite",
-    category: "reaction",
-    use: "<quote|reply|tag>",
-    filename: __filename,
-    desc: "send Anime bite reaction.",
-  },
-  async (bot, text, { cmdName }) => {
-    await sendAnimeReaction(bot, cmdName, "bitten to", "bitten to everyone.");
-  }
-);
-//---------------------------------------------------------------------------
-smd(
-  {
-    pattern: "blush",
-    category: "reaction",
-    use: "<quote|reply|tag>",
-    filename: __filename,
-    desc: "send Anime blush reaction.",
-  },
-  async (bot, text, { cmdName }) => {
-    await sendAnimeReaction(bot, cmdName, "blushed to", "blushed to everyone.");
-  }
-);
-//---------------------------------------------------------------------------
-smd(
-  {
-    pattern: "punch",
-    category: "reaction",
-    use: "<quote|reply|tag>",
-    filename: __filename,
-    desc: "send Anime punch reaction.",
-  },
-  async (bot, text, { cmdName }) => {
-    await sendAnimeReaction(bot, "kick", "punched to", "punched everyone.");
-  }
-);
-//---------------------------------------------------------------------------
-smd(
-  {
-    pattern: "pat",
-    category: "reaction",
-    use: "<quote|reply|tag>",
-    filename: __filename,
-    desc: "send Anime pated reaction.",
-  },
-  async (bot, text, { cmdName }) => {
-    await sendAnimeReaction(
-      bot,
-      cmdName,
-      "patted with",
-      "patted with everyone."
-    );
-  }
-);
-//---------------------------------------------------------------------------
-smd(
-  {
-    pattern: "kiss",
-    category: "reaction",
-    use: "<quote|reply|tag>",
-    filename: __filename,
-    desc: "send Anime kiss reaction.",
-  },
-  async (bot, text, { cmdName }) => {
-    await sendAnimeReaction(
-      bot,
-      cmdName,
-      "kissed with",
-      "kissed with everyone."
-    );
-  }
-);
-//---------------------------------------------------------------------------
-smd(
-  {
-    pattern: "kill",
-    category: "reaction",
-    use: "<quote|reply|tag>",
-    filename: __filename,
-    desc: "send Anime kill reaction.",
-  },
-  async (bot, text, { cmdName }) => {
-    await sendAnimeReaction(bot, cmdName, "kill ", "kill everyone over here");
-  }
-);
-//---------------------------------------------------------------------------
-smd(
-  {
-    pattern: "happy",
-    category: "reaction",
-    use: "<quote|reply|tag>",
-    filename: __filename,
-    desc: "send Anime happy reaction.",
-  },
-  async (bot, text, { cmdName }) => {
-    await sendAnimeReaction(
-      bot,
-      "dance",
-      "feel happy with",
-      "feel happy with everyone"
-    );
-  }
-);
-//---------------------------------------------------------------------------
-smd(
-  {
-    pattern: "dance",
-    category: "reaction",
-    use: "<quote|reply|tag>",
-    filename: __filename,
-    desc: "send Anime dance reaction.",
-  },
-  async (bot, text, { cmdName }) => {
-    await sendAnimeReaction(
-      bot,
-      cmdName,
-      "dance with",
-      "dance with everyone over here"
-    );
-  }
-);
-//---------------------------------------------------------------------------
-smd(
-  {
-    pattern: "yeet",
-    category: "reaction",
-    use: "<quote|reply|tag>",
-    filename: __filename,
-    desc: "send Anime yeet reaction.",
-  },
-  async (bot, text, { cmdName }) => {
-    await sendAnimeReaction(bot, cmdName, "yeeted to", "yeeted with everyone");
-  }
-);
-//---------------------------------------------------------------------------
-smd(
-  {
-    pattern: "wink",
-    category: "reaction",
-    use: "<quote|reply|tag>",
-    filename: __filename,
-    desc: "send Anime wink reaction.",
-  },
-  async (bot, text, { cmdName }) => {
-    await sendAnimeReaction(
-      bot,
-      cmdName,
-      "winked with",
-      "winked with everyone"
-    );
-  }
-);
-//---------------------------------------------------------------------------
-smd(
-  {
-    pattern: "slap",
-    category: "reaction",
-    use: "<quote|reply|tag>",
-    filename: __filename,
-    desc: "send Anime slap reaction.",
-  },
-  async (bot, text, { cmdName }) => {
-    await sendAnimeReaction(bot, cmdName, "slap to", "slap to everyone");
-  }
-);
-//---------------------------------------------------------------------------
-smd(
-  {
-    pattern: "bonk",
-    category: "reaction",
-    use: "<quote|reply|tag>",
-    filename: __filename,
-    desc: "send Anime bonk reaction.",
-  },
-  async (bot, text, { cmdName }) => {
-    await sendAnimeReaction(bot, cmdName, "bonked to", "bonked to everyone");
-  }
-);
-//---------------------------------------------------------------------------
-smd(
-  {
-    pattern: "bully",
-    category: "reaction",
-    use: "<quote|reply|tag>",
-    filename: __filename,
-    desc: "send Anime bully reaction.",
-  },
-  async (bot, text, { cmdName }) => {
-    await sendAnimeReaction(bot, cmdName, "bullied to", "bullied to everyone");
-  }
-);
-//---------------------------------------------------------------------------
-smd(
-  {
-    pattern: "cringe",
-    category: "reaction",
-    use: "<quote|reply|tag>",
-    filename: __filename,
-    desc: "send Anime cringe reaction.",
-  },
-  async (bot, text, { cmdName }) => {
-    await sendAnimeReaction(bot, cmdName, "cringed to", "cringed to everyone");
-  }
-);
-//---------------------------------------------------------------------------
-smd(
-  {
-    pattern: "cuddle",
-    category: "reaction",
-    use: "<quote|reply|tag>",
-    filename: __filename,
-    desc: "send Anime cuddle reaction.",
-  },
-  async (bot, text, { cmdName }) => {
-    await sendAnimeReaction(
-      bot,
-      cmdName,
-      "cuddled with",
-      "cuddled with everyone"
-    );
-  }
-);
+});
