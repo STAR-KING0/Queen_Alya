2de24d1d29ff774e1514cf093aaf0f87e7147828
STAR-KING0 <kingdavid23565@gmail.com>
2024-09-02 12:52:22 +0100

    Update config.js

--- config.js
+++ config.js
@@ -57,7 +57,7 @@ module.exports = {
   errorChat: process.env.ERROR_CHAT || "",
   KOYEB_API: process.env.KOYEB_API || "false",
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
-  OPENAI_API_KEY: process.env.OPENAI_API_KEY || "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",
+  OPENAI_API_KEY: process.env.OPENAI_API_KEY || "sk-qxpQpJ-Mq1E6PIJ0q10PMNpQ2dZHJLIpNuLxv_SXdgT3BlbkFJuJuesP4lz39WGcqglcdoYDxTRUrkvHZGeNX_-klnsA",
   HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
   HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || "",
   antilink_values: process.env.ANTILINK_VALUES || "all",
